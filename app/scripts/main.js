c56d8ac0d80054399d73a6a201e5dfe4c83dcc04
maswhitaker <masforwhi@yahoo.com>
2015-03-17 11:26:24 -0400

    New file input

---
+++ app/scripts/main.js
@@ -0,0 +1,70 @@
+$("#B").click(function(){
+  setInterval(function(){
+      $("#B").css("border-top-style", "solid");
+  }, 1000);
+  setInterval(function(){
+      $("#B").css("border-right-style", "solid");
+  }, 2000);
+  setInterval(function(){
+      $("#B").css("border-bottom-style", "double");
+  }, 1000);
+  setInterval(function(){
+      $("#B").css("border-left-style", "double");
+  }, 2000);
+});
+$("#E").click(function(){
+  setInterval(function(){
+      $("#E").css("border-top-style", "solid");
+  }, 1000);
+  setInterval(function(){
+      $("#E").css("border-right-style", "solid");
+  }, 2000);
+  setInterval(function(){
+      $("#E").css("border-bottom-style", "double");
+  }, 1000);
+  setInterval(function(){
+      $("#E").css("border-left-style", "double");
+  }, 2000);
+});
+$("#C").click(function(){
+  setInterval(function(){
+      $("#C").css("border-top-style", "solid");
+  }, 1000);
+  setInterval(function(){
+      $("#C").css("border-right-style", "solid");
+  }, 2000);
+  setInterval(function(){
+      $("#C").css("border-bottom-style", "double");
+  }, 1000);
+  setInterval(function(){
+      $("#C").css("border-left-style", "double");
+  }, 2000);
+});
+$("#C2").click(function(){
+  setInterval(function(){
+      $("#C2").css("border-top-style", "solid");
+  }, 1000);
+  setInterval(function(){
+      $("#C2").css("border-right-style", "solid");
+  }, 2000);
+  setInterval(function(){
+      $("#C2").css("border-bottom-style", "double");
+  }, 1000);
+  setInterval(function(){
+      $("#C2").css("border-left-style", "double");
+  }, 2000);
+});
+$("#A").click(function(){
+  setInterval(function(){
+      $("#A").css("border-top-style", "solid");
+  }, 1000);
+  setInterval(function(){
+      $("#A").css("border-right-style", "solid");
+  }, 2000);
+  setInterval(function(){
+      $("#A").css("border-bottom-style", "double");
+  }, 1000);
+  setInterval(function(){
+      $("#A").css("border-left-style", "double");
+  }, 2000);
+});
